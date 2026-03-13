5da7c1dfdb1011f9d8e1ef1dc3b1209ebc1723a1
Daniel Brito <daniel.rbrito96@gmail.com>
2026-03-13 13:16:17 -0300

    Update README and Playwright config for improved CI integration and error handling

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,35 +2,26 @@ import { defineConfig, devices } from "@playwright/test";
 import dotenv from "dotenv";
 
 dotenv.config({ path: ".env.saucedemo" });
-console.log("BASE_URL:", process.env.BASE_URL);
+quiet: true;
 
 // Environment file validation
 if (!process.env.BASE_URL) {
   throw new Error("BASE_URL not defined. Set it locally in .env.saucedemo or in GitHub Actions.");
 }
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: "./tests-e2e",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter:[['html', { open: 'always' }]],
+  reporter: process.env.CI
+  ? [['dot'], ['html', { open: 'never' }]]
+  : [['html', { open: 'always' }]],
 
   use: {
     baseURL: process.env.BASE_URL,
-    trace: "on",
+    trace: "on-first-retry",
     screenshot: "on",
   },
 
